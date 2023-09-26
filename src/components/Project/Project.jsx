abaa3eba780dd927f9c472b535a4455c78d9c2cd
mubzie <85705074+mubzie@users.noreply.github.com>
2023-09-26 14:55:38 +0100

    restyled

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,25 +5,69 @@ const Project = () => {
     <>
       <section className={styles.project}>
         <div className={styles.title}>Projects </div>
-        <div className={styles.details}>
-          <div className={styles.head}>
-            <div>MovieBox</div>
-            <div> livelink</div>
+        <div className={styles.projectContainer}>
+          <div className={styles.details}>
+            <div className={styles.head}>
+              <div>Profile page</div>
+              <div> livelink</div>
+            </div>
+            <p className={styles.para}>
+              Spearheaded the building of the frontend of the website,
+              integrating the backend API endpoints with the frontend and
+              implementing the UI design for the landing pages, login pages.
+              Nextjs ChakraUI React Query Collate Africa Live Collate.Africa was
+              a groundbreaking project aimed at empowering Nigerian citizens to
+              actively participate in the electoral process by providing a
+              seamless platform for submitting and accessing election results
+              during the last conducted elections
+            </p>
+            <div className={styles.stackContainer}>
+              <span className={styles.stack}>javaScript</span>
+              <span className={styles.stack}>HTML</span>
+              <span className={styles.stack}>CSS</span>
+            </div>
           </div>
-          <p className={styles.para}>
-            Spearheaded the building of the frontend of the website, integrating
-            the backend API endpoints with the frontend and implementing the UI
-            design for the landing pages, login pages. Nextjs ChakraUI React
-            Query Collate Africa Live Collate.Africa was a groundbreaking
-            project aimed at empowering Nigerian citizens to actively
-            participate in the electoral process by providing a seamless
-            platform for submitting and accessing election results during the
-            last conducted elections
-          </p>
-          <div className={styles.stackContainer}>
-            <span className={styles.stack}>javascript</span>
-            <span className={styles.stack}>html</span>
-            <span className={styles.stack}>css</span>
+          <div className={styles.details}>
+            <div className={styles.head}>
+              <div>MovieBox</div>
+              <div> livelink</div>
+            </div>
+            <p className={styles.para}>
+              Spearheaded the building of the frontend of the website,
+              integrating the backend API endpoints with the frontend and
+              implementing the UI design for the landing pages, login pages.
+              Nextjs ChakraUI React Query Collate Africa Live Collate.Africa was
+              a groundbreaking project aimed at empowering Nigerian citizens to
+              actively participate in the electoral process by providing a
+              seamless platform for submitting and accessing election results
+              during the last conducted elections
+            </p>
+            <div className={styles.stackContainer}>
+              <span className={styles.stack}>React</span>
+              <span className={styles.stack}>Context API</span>
+              <span className={styles.stack}>CSS</span>
+            </div>
+          </div>
+          <div className={styles.details}>
+            <div className={styles.head}>
+              <div>Drag&Drop</div>
+              <div> livelink</div>
+            </div>
+            <p className={styles.para}>
+              Spearheaded the building of the frontend of the website,
+              integrating the backend API endpoints with the frontend and
+              implementing the UI design for the landing pages, login pages.
+              Nextjs ChakraUI React Query Collate Africa Live Collate.Africa was
+              a groundbreaking project aimed at empowering Nigerian citizens to
+              actively participate in the electoral process by providing a
+              seamless platform for submitting and accessing election results
+              during the last conducted elections
+            </p>
+            <div className={styles.stackContainer}>
+              <span className={styles.stack}>React</span>
+              <span className={styles.stack}>Firebase</span>
+              <span className={styles.stack}>Context API</span>
+            </div>
           </div>
         </div>
       </section>
