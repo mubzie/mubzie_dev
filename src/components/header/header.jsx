b516d88d50f74072cfaaa312fc3f22eb187745ee
mubzie <rabiumubarak92@gmail.com>
2025-05-18 17:37:13 +0100

    style: enhance responsive design across multiple components with media queries

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,13 +3,11 @@ import Background from "../Background/Background";
 
 const Header = () => {
   return (
-    <div>
-      <div className={styles.header}>
-        <h3 className={styles.name}>
-          Mubarak Rabiu - A multidisciplinary designer.
-        </h3>
-        <Background />
-      </div>
+    <div className={styles.header}>
+      <h3 className={styles.name}>
+        Mubarak Rabiu - A multidisciplinary designer.
+      </h3>
+      <Background />
     </div>
   );
 };
