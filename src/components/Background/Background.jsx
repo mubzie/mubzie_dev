2b22b6ad5083cf77d442068f01c9b20c3ad4dd0a
mubzie <rabiumubarak92@gmail.com>
2025-05-19 10:59:39 +0100

    style: update background text for clarity and improved readability

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -9,17 +9,17 @@ function Background() {
       "_blank"
     );
   };
-  
+
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Background</h3>
       <p className={styles.text}>
-        I’m a product designer with expertise in graphic design, branding, and
-        digital experiences. Passionate about solving real-world problems
-        through user-centered design, my work blends creativity with
-        practicality. This portfolio showcases my problem-solving
-        approach—turning challenges into elegant solutions. I love collaborating
-        with teams that value innovation and execution.
+        I’m a product designer with hands-on experience in graphic design, brand
+        identity, and digital experiences. My work is rooted in solving
+        real-world problems through thoughtful, user-centered design—where
+        creativity meets functionality. This portfolio showcases my
+        problem-solving approach—turning challenges into elegant solutions. I
+        love collaborating with teams that value innovation and execution.
       </p>
       <Button
         onClick={handleButtonClick}
