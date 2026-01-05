ae4b43f9ab65941f1b4069b4bb04c9c9e7f65649
mubzie <rabiumubarak92@gmail.com>
2026-01-05 08:44:57 +0100

    Refactor App component: integrate WorkCard and ExperienceCard; add ContactForm with email functionality; remove About and Project components; introduce App.module.css for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-// import Header from "./components/header/header";
-import About from "./components/About/About";
-import Project from "./components/Project/Project";
-// import { ChevronRight } from "lucide-react";
-// import { SplinePointer } from "lucide-react";
-// import img from "./assets/Images/img1.png";
+import { ChevronRight } from "lucide-react";
+import { SplinePointer } from "lucide-react";
+import img from "./assets/Images/img1.png";
+import styles from "./App.module.css";
 
 // import Button from "./components/Button/Button";
-// import WorkCard from "./components/WorkCard/WorkCard";
-// import ExperienceCard from "./components/ExperienceCard/ExperienceCard";
-import ContantForm from "./components/ContantForm/ContactForm";
+import WorkCard from "./components/WorkCard/WorkCard";
+import ExperienceCard from "./components/ExperienceCard/ExperienceCard";
+import ContantForm from "./components/ContactForm/ContactForm";
 
 function App() {
   return (
-    <>
-      {/* <Header /> */}
-      <About />
-      {/* <WorkCard cardTitle="UI exploration" icon={SplinePointer} /> */}
-      {/* <ExperienceCard
+    <div className={styles.appWrapper}>
+      <WorkCard cardTitle="UI exploration" icon={SplinePointer} />
+      <ExperienceCard
         src={img}
         alt="alt"
         company="Acme"
         designation="product designer"
         duration="Mar 2025 - Present"
         description="Tasked with designing intuitive user and admin dashboards focused on clarity and brand consistency."
-      /> */}
+      />
       <ContantForm />
-      <Project />
-    </>
+    </div>
   );
 }
 
