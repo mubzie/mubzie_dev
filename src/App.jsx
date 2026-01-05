56861059f4a3a1b7f1f3edcdeeaddb53d5132f91
mubzie <rabiumubarak92@gmail.com>
2026-01-05 22:18:46 +0100

    feat: added experience section

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,15 @@ import ExperienceCard from "./components/ExperienceCard/ExperienceCard";
 import ContantForm from "./components/ContactForm/ContactForm";
 import MainLockup from "./sections/MainLockup/MainLockup";
 import WorkSection from "./sections/WorkSection/WorkSection";
+import ExperienceSection from "./sections/ExperienceSection/ExperienceSection";
 
 function App() {
   return (
     <div className={styles.appWrapper}>
       <MainLockup />
       <WorkSection />
-      {/* <WorkCard cardTitle="UI exploration" icon={SplinePointer} />
-      <ExperienceCard
-        src={img}
-        alt="alt"
-        company="Acme"
-        designation="product designer"
-        duration="Mar 2025 - Present"
-        description="Tasked with designing intuitive user and admin dashboards focused on clarity and brand consistency."
-      />
-      <ContantForm /> */}
+      <ExperienceSection />
+      {/* <ContantForm /> */}
     </div>
   );
 }
--- src/sections/ExperienceSection/ExperienceSection.jsx
+++ src/sections/ExperienceSection/ExperienceSection.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import styles from "./ExperienceSection.module.css";
+import ExperienceCard from "../../components/ExperienceCard/ExperienceCard";
+import img from "../../assets/Images/img1.png";
 
 function ExperienceSection() {
   return (
-    <div>ExperienceSection</div>
-  )
+    <div>
+      <h4 className={styles.experienceTitle}>Featured work</h4>
+      <p className={styles.experienceDescription}>
+        I am a product designer with a multidisciplinary background in graphic
+        design, brand identity, and digital experiences.
+      </p>
+      <div className={styles.experienceCardWrapper}>
+        <ExperienceCard
+          src={img}
+          alt="alt"
+          company="Acme"
+          designation="product designer"
+          duration="Mar 2025 - Present"
+          description="Tasked with designing intuitive user and admin dashboards focused on clarity and brand consistency."
+        />
+        <ExperienceCard
+          src={img}
+          alt="alt"
+          company="Acme"
+          designation="product designer"
+          duration="Mar 2025 - Present"
+          description="Tasked with designing intuitive user and admin dashboards focused on clarity and brand consistency."
+        />
+        <ExperienceCard
+          src={img}
+          alt="alt"
+          company="Acme"
+          designation="product designer"
+          duration="Mar 2025 - Present"
+          description="Tasked with designing intuitive user and admin dashboards focused on clarity and brand consistency."
+        />
+      </div>
+    </div>
+  );
 }
 
-export default ExperienceSection
+export default ExperienceSection;
