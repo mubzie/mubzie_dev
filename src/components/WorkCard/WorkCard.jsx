485fdf8ccc95ca66afed7cbc141ef3b47348a0dd
mubzie <rabiumubarak92@gmail.com>
2026-01-10 10:24:09 +0100

    added click handler to work card

--- src/components/WorkCard/WorkCard.jsx
+++ src/components/WorkCard/WorkCard.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import styles from "./WorkCard.module.css";
 
-function WorkCard({ icon, cardTitle }) {
+function WorkCard({ icon, cardTitle, onClick }) {
   const Icon = icon;
 
   return (
-    <div className={styles.WorkCardWrapper}>
+    <div onClick={onClick} className={styles.WorkCardWrapper}>
       <div className={styles.textWrapper}>
         <div className={styles.backdrop}></div>
         <span>{Icon && <Icon />}</span>
@@ -18,6 +18,7 @@ function WorkCard({ icon, cardTitle }) {
 WorkCard.propTypes = {
   icon: PropTypes.element,
   cardTitle: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default WorkCard;
