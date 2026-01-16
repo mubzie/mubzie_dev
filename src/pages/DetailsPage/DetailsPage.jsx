6ecaa6907e22884ed04b5899d6db4a5fdc72baa0
mubzie <rabiumubarak92@gmail.com>
2026-01-16 18:58:59 +0100

    feat: working on detail page

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,11 +1,38 @@
 import { useParams } from "react-router-dom";
+import { usePortfolioData } from "../../hooks/usePortfolioData";
+import styles from "./DetailsPage.module.css";
 
 function DetailsPage() {
   const { slug } = useParams();
+  const { getCategoryBySlug, getProjectByCategory } = usePortfolioData();
 
-  console.log(slug);
+  const category = getCategoryBySlug(slug);
+  const projects = getProjectByCategory(slug);
 
-  return <div>DetailsPage {`${slug} - ${0}`}</div>;
+  console.log(category);
+  console.log(projects);
+
+  const categoryName = category.name;
+  // const categoryDescription = category
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h3 className={styles.header}>{categoryName}</h3>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat,
+          aperiam adipisci? Quo voluptatibus magnam nobis, quae doloremque saepe
+          vero maxime.
+        </p>
+
+        {projects.map((project) => (
+          <div key={project.id} className={styles.imageContainer}>
+            <img src={project.images[0].imagePath} />
+            <img src={project.images[1].imagePath} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default DetailsPage;
